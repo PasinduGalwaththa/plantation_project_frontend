760915af077b1373917be1e7d76f0f48e91d387a
PasinduGalwaththa <nawodyapasindu@gmail.com>
2023-07-14 00:03:52 +0530

    successfully completed delete method

--- src/Pages/CollectorDashboard/SetArrivals.js
+++ src/Pages/CollectorDashboard/SetArrivals.js
@@ -1,275 +1,242 @@
-import React, {useState,Fragment,useEffect}from "react";
-import './UpdateForm.css';
+import React, { useState, Fragment, useEffect } from "react";
+import "./UpdateForm.css";
 import Navbar from "../../Components/Navbar/Navbar";
-import moment from 'moment';
+import moment from "moment";
 import FormControl from "react-bootstrap/FormControl";
 import Button from "react-bootstrap/Button";
 //import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 //import InputGroup from "react-bootstrap/InputGroup";
 //import Row from "react-bootstrap/Row";
-import FloatingLabel from 'react-bootstrap/esm/FloatingLabel';
-import FormGroup from 'react-bootstrap/esm/FormGroup';
-import MaskedFormControl  from 'react-bootstrap-maskedinput';
+import FloatingLabel from "react-bootstrap/esm/FloatingLabel";
+import FormGroup from "react-bootstrap/esm/FormGroup";
+import MaskedFormControl from "react-bootstrap-maskedinput";
 import axios from "axios";
-import "./SetArrivalscss.css"
+import "./SetArrivalscss.css";
 //import data from "./mock-data.json"
 import ReadOnlyRow from "../../Components/ReadOnlyRow";
 import EditableRow from "../../Components/EditableRow";
 
-export const SetArrivals =()=>{
-    const [currentDay, setCurrentDay] = useState("");// Get the current day
-    const [contacts, setContacts] = useState(new Array(0));
-    const [addFormData,setAddFormData]=useState({
-      
-      collectionpointname:'',
-      collectionpoint:'',
-      arrivaltime:'',
-      day:'',
-    });
+export const SetArrivals = () => {
+  const [currentDay, setCurrentDay] = useState(""); // Get the current day
+  const [contacts, setContacts] = useState(new Array(0));
+  const [addFormData, setAddFormData] = useState({
+    collectionpointname: "",
+    collectionpoint: "",
+    arrivaltime: "",
+    day: "",
+  });
 
-    useEffect(() => {
-      // Get the current day when the component mounts
-      const day = moment().format("dddd");
-      setCurrentDay(day);
-    }, []);
+  useEffect(() => {
+    // Get the current day when the component mounts
+    const day = moment().format("dddd");
+    setCurrentDay(day);
+  }, []);
 
-    const [editFormData,setEditFormData]=useState({
-      
-      collectionpointname:'',
-      collectionpoint:'',
-      arrivaltime:'',
-      day:'',
+  const [editFormData, setEditFormData] = useState({
+    collectionpointname: "",
+    collectionpoint: "",
+    arrivaltime: "",
+    day: "",
+  });
 
-    })
+  const [editContactId, setEditContactId] = useState(null);
+  const [response, setResponse] = useState();
 
-    const [editContactId,setEditContactId]=useState(null);
+  const handleAddFormChange = (event) => {
+    const fieldName = event.target.getAttribute("name");
+    const fieldValue = event.target.value;
 
-    const handleAddFormChange=(event)=>{
-      
-     
+    const newFormData = { ...addFormData };
+    newFormData[fieldName] = fieldValue;
 
-      const fieldName=event.target.getAttribute('name');
-      const fieldValue=event.target.value;
+    setAddFormData(newFormData);
 
-      const newFormData={...addFormData};
-      newFormData[fieldName]=fieldValue;
+    console.log(newFormData);
+    event.preventDefault();
+  };
 
-      setAddFormData(newFormData);
-      
-      console.log(newFormData);
-      event.preventDefault();
-    }
+  const handleEditFormChange = (event) => {
+    event.preventDefault();
 
-    const handleEditFormChange = (event)=>{
-      event.preventDefault();
+    const fieldName = event.target.getAttribute("name");
+    const fieldValue = event.target.value;
 
-      const fieldName=event.target.getAttribute('name');
-      const fieldValue=event.target.value;
+    const newFormData = { ...editFormData };
+    newFormData[fieldName] = fieldValue;
 
-      const newFormData={...editFormData};
-      newFormData[fieldName]=fieldValue;
+    setEditFormData(newFormData);
+  };
 
-      setEditFormData(newFormData);
+  const handleSubmit = async (event) => {
+    const form = event.currentTarget;
 
-      
-    }
+    //console.log(addFormData);
+    event.preventDefault();
 
-    const handleSubmit = async (event) => {
-      const form = event.currentTarget;
-      
-      //console.log(addFormData);
-      event.preventDefault();
-
-      const newContact={
-        
-        
-        collectionpointname:addFormData.collectionpointname,
-        collectionpoint:addFormData.collectionpoint,
-        arrivaltime:addFormData.arrivaltime,
-        day:currentDay,
-
-      }
-      
-      console.log(newContact);
-
-      axios.post('http://127.0.0.1:8000/setarrivals/' , newContact )
-      .then (res => {
-      alert(res.data)
-      
-      
-    }).catch (err => {
-      console.error(err)
-    })
-      const newContacts= [...contacts,newContact];
-      setContacts(newContacts);
-
-
-
-      setAddFormData({
-        collectionpointname: "",
-        collectionpoint: "",
-        arrivaltime: "",
-        day: "", // Clear the 'day' property
-      });
-      
-
-     
+    const newContact = {
+      collectionpointname: addFormData.collectionpointname,
+      collectionpoint: addFormData.collectionpoint,
+      arrivaltime: addFormData.arrivaltime,
+      day: currentDay,
     };
 
-    const handleEditformSubmit=(event)=>{
-      event.preventDefault();
+    console.log(newContact);
 
-      const editedContact={
-        id:editContactId,
-        collectionpointname:editFormData.collectionpointname,
-        collectionpoint:editFormData.collectionpoint,
-        arrivaltime:editFormData.arrivaltime,
-        day:editFormData.day
-      }
-      const newContacts=[...contacts];
-      const index = contacts.findIndex((contact)=>contact.id===editContactId);
-      newContacts[index]= editedContact;
-      setContacts(newContacts);
-      setEditContactId(null);
-    }
-    
-    const handleEditClick=(event,contact)=>{
-      event.preventDefault();
-      setEditContactId(contact.id);
-
-      const formValues = {
-        collectionpointname:contact.collectionpointname,
-        collectionpoint:contact.collectionpoint,
-        arrivaltime:contact.arrivaltime,
-        day:contact.day
-
-        
-  
-      }
-      setEditFormData(formValues);
-    }
-
-    const handleCancelClick=()=>{
-      setEditContactId(null);
-    }
-
-    const handleDeleteClick=(contactId)=>{
-      const newContacts=[...contacts];
-
-      const index=contacts.findIndex((contact)=>contact.id===contactId);
-      newContacts.splice(index,1);
-
-      setContacts(newContacts);
-
-    }
-    useEffect(() => {
-      
-      axios.get('http://127.0.0.1:8000/setarrivals/')
-      .then(res => {
-          console.log(res.data)
-          setContacts(res.data)
-          
+    axios
+      .post("http://127.0.0.1:8000/setarrivals/", newContact)
+      .then((res) => {
+        console.log(res.data)
+        setResponse(res.data);
       })
-  
-    
-  }, [])
+      .catch((err) => {
+        console.error(err);
+      });
+    const newContacts = [...contacts, newContact];
+    setContacts(newContacts);
 
-    
-    
+    setAddFormData({
+      collectionpointname: "",
+      collectionpoint: "",
+      arrivaltime: "",
+      day: "", // Clear the 'day' property
+    });
+  };
 
-    
-    
-    return(
-        <div className="form">
-      <div><Navbar /></div>
+  const handleEditformSubmit = (event) => {
+    event.preventDefault();
+
+    const editedContact = {
+      id: editContactId,
+      collectionpointname: editFormData.collectionpointname,
+      collectionpoint: editFormData.collectionpoint,
+      arrivaltime: editFormData.arrivaltime,
+      day: editFormData.day,
+    };
+    const newContacts = [...contacts];
+    const index = contacts.findIndex((contact) => contact.id === editContactId);
+    newContacts[index] = editedContact;
+    setContacts(newContacts);
+    setEditContactId(null);
+  };
+
+  const handleEditClick = (event, contact) => {
+    event.preventDefault();
+    setEditContactId(contact.id);
+
+    const formValues = {
+      collectionpointname: contact.collectionpointname,
+      collectionpoint: contact.collectionpoint,
+      arrivaltime: contact.arrivaltime,
+      day: contact.day,
+    };
+    setEditFormData(formValues);
+  };
+
+  const handleCancelClick = () => {
+    setEditContactId(null);
+  };
+
+  const handleDeleteClick = (contactId) => {
+    const newContacts = [...contacts];
+
+    const index = contacts.findIndex((contact) => contact.id === contactId);
+    newContacts.splice(index, 1);
+    axios
+      .delete(`http://127.0.0.1:8000/setarrivals/delete/${contactId}`)
+      .then((res) => {
+        console.log(res.data)
+        setContacts(newContacts);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+  useEffect(() => {
+    axios.get("http://127.0.0.1:8000/setarrivals/").then((res) => {
+      console.log(res.data);
+      setContacts(res.data);
+    });
+  }, [response]);
+
+  return (
+    <div className="form">
       <div>
-      <div className="div1"><h1>Set Arrivals by collector</h1></div>
-      <div>Current Day: {currentDay}</div>
-     
-      <div className="addCollectionPoint">
-        <h2>Add a collection point</h2>
-        <form onSubmit={handleSubmit}>
-          <input
-          type="text"
-          name="collectionpointname"
-          required
-          placeholder="enter"
-          onChange={handleAddFormChange}
-          />
-          <input
-          type="number"
-          name="collectionpoint"
-          required
-          placeholder="enter cp"
-          onChange={handleAddFormChange}
-          />
-
-          <input
-          type="time"
-          name="arrivaltime"
-          required
-          placeholder="enter time"
-          onChange={handleAddFormChange}
-          />
-          <button type="submit">Add</button>
-        </form>
-
+        <Navbar />
       </div>
-      
-      <div className="app-container">
-        <form onSubmit={handleEditformSubmit}>
+      <div>
+        <div className="div1">
+          <h1>Set Arrivals by collector</h1>
+        </div>
+        <div>Current Day: {currentDay}</div>
 
-        
-        <table>
-          <thead>
-          <tr>
-            <th>collectionpointname</th>
-            <th>collectionpoint</th>
-            <th>arrivaltime</th>
-            <th>Actions</th>
-          </tr>
-          </thead>
-          <tbody>
-            {contacts.map((contact)=>(
-              <React.Fragment key={contact.id}>
-                {editContactId===contact.id?(
-                <EditableRow  
-                editFormData={editFormData} 
-                handleEditFormChange={handleEditFormChange}
-                handleCancelClick={handleCancelClick}
-                />)
-                :(<ReadOnlyRow
-                 contact={contact} 
-                 handleEditClick={handleEditClick}
-                 handleDeleteClick={handleDeleteClick}/>)}
-             
-              
-            </React.Fragment>
-            )
-            
-           
+        <div className="addCollectionPoint">
+          <h2>Add a collection point</h2>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              name="collectionpointname"
+              required
+              placeholder="enter"
+              onChange={handleAddFormChange}
+            />
+            <input
+              type="number"
+              name="collectionpoint"
+              required
+              placeholder="enter cp"
+              onChange={handleAddFormChange}
+            />
 
-       
-            
-            
-             
-            )}
-           
-            
-          </tbody>
-        </table>
-        </form>
-        
+            <input
+              type="time"
+              name="arrivaltime"
+              required
+              placeholder="enter time"
+              onChange={handleAddFormChange}
+            />
+            <button type="submit">Add</button>
+          </form>
+        </div>
+
+        <div className="app-container">
+          <form onSubmit={handleEditformSubmit}>
+            <table>
+              <thead>
+                <tr>
+                  <th>collectionpointname</th>
+                  <th>collectionpoint</th>
+                  <th>arrivaltime</th>
+                  <th>Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                {contacts
+                  .filter((contact) => contact.day === currentDay)
+                  .map((contact,index) => (
+                    <React.Fragment key={index}>
+                      {editContactId === contact.id ? (
+                        <EditableRow
+                          editFormData={editFormData}
+                          handleEditFormChange={handleEditFormChange}
+                          handleCancelClick={handleCancelClick}
+                        />
+                      ) : (
+                        <ReadOnlyRow
+                          contact={contact}
+                          handleEditClick={handleEditClick}
+                          handleDeleteClick={handleDeleteClick}
+                        />
+                      )}
+                    </React.Fragment>
+                  ))}
+              </tbody>
+            </table>
+          </form>
+        </div>
       </div>
-
-      </div>
-     
-      
-     
-      
     </div>
-    );
-}
+  );
+};
 
 export default SetArrivals;
